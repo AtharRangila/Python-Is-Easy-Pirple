b5b4e24970b31afd8f9b50c007c44ccef9336d25
AtharRangila <55705742+AtharRangila@users.noreply.github.com>
2021-01-28 02:33:57 +0530

    Create HomeworkAssignment1.py

---
+++ HomeworkAssignment1.py
@@ -0,0 +1,13 @@
+#This file is created for the sole purpose of helping out
+
+
+#Homework Assignment 1 is based on variables.
+#You have to create 3 variables.
+
+Artist = "Shallou" #Variable Artist was created
+Genre = "Dance/Electronic"  #Variable Genre was created
+DurationInSeconds = 234     #Variable DurationInSeconds was created
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
